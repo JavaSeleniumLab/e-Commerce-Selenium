513d17664432a34db2c9680f7b27e00ef86e02c1
Alina-lab777 <elliekuch@gmail.com>
2020-09-29 18:01:11 -0400

    Added Testcases Sep 29

--- src/test/java/POM/Pages/basePage.java
+++ src/test/java/POM/Pages/basePage.java
@@ -2,56 +2,106 @@ package POM.Pages;
 
 import WebDriverPakg.DriverWrapper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
-public class basePage {
+import java.util.List;
+import java.util.Set;
+
+public class basePage extends DriverWrapper {
+
+    public List<WebElement> findWebElements (By locator) {
+        return getDriver().findElements(locator);
+    }
+
+    public WebElement findWebElement (By locator){
+        return DriverWrapper.getDriver().findElement(locator);
+    }
 
     public void type(By locator, String data) {
-        DriverWrapper.getDriver().findElement(locator).sendKeys(data);
+        findWebElement(locator).sendKeys(data);
     }
 
     public void clickThis(By locator) {
-        DriverWrapper.getDriver().findElement(locator).click();
+        findWebElement(locator).click();
     }
 
     public boolean isElementDisplayed(By locator) {
-        return DriverWrapper.getDriver().findElement(locator).isDisplayed();
+        return findWebElement(locator).isDisplayed();
     }
 
     // isSelected
     public boolean isSelected (By locator){
-        return DriverWrapper.getDriver().findElement(locator).isSelected();
-
+        return findWebElement(locator).isSelected();
     }
 
     // isEnabled
     public boolean isEnabled (By locator){
-        return DriverWrapper.getDriver().findElement(locator).isEnabled();
+        return findWebElement(locator).isEnabled();
     }
 
     // getText
     public String getText(By locator){
-       String text= DriverWrapper.getDriver().findElement(locator).getText();
+       String text= findWebElement(locator).getText();
        return text;
     }
 
     // getAttribute
     public String getAttribute(By locator, String attName){
-        return DriverWrapper.getDriver().findElement(locator).getAttribute(attName);
+        return findWebElement(locator).getAttribute(attName);
     }
+
     // getTitle
     public String getTitle(){
-        return DriverWrapper.getDriver().getTitle();
+        return getDriver().getTitle();
     }
 
     // getCurrentUrl
     public String getCurrentUrl(){
-        return DriverWrapper.getDriver().getCurrentUrl();
+        return getDriver().getCurrentUrl();
     }
 
     //
     public String getValue(){
-        return DriverWrapper.getDriver().getTitle(); 
+        return getDriver().getTitle();
     }
 
+    public void selectText (By locator, String text){
+       WebElement textLocator= findWebElement(locator);
+        Select s1 = new Select(textLocator);
+        s1.selectByVisibleText(text);
+    }
 
+    public void selectValueAttr (By locator, String value){
+        WebElement valueLocator=findWebElement(locator);
+        Select s2= new Select(valueLocator);
+        s2.selectByValue(value);
+    }
+
+    public void selectIndex (By locator, int index){
+        WebElement indexLocator = findWebElement(locator);
+        Select s3 = new Select(indexLocator);
+        s3.deselectByIndex(index);
+    }
+
+    public String getWindowHandle(){
+       return getDriver().getWindowHandle();
+    }
+
+    public Set<String> getWindowHandles(){
+        return getDriver().getWindowHandles();
+    }
+
+    public void switchToNewWindow(String handle){
+        getDriver().switchTo().window(handle);
+    }
+
+    public void switchToFrame (String id){
+        getDriver().switchTo().frame(id);
+    }
+
+    public void switchToWebFrame (By locator){
+        WebElement webFrameLocator=findWebElement(locator);
+        getDriver().switchTo().frame(webFrameLocator);
+    }
 }
--- src/test/java/POM/Pages/landingPage.java
+++ src/test/java/POM/Pages/landingPage.java
@@ -3,13 +3,33 @@ import org.openqa.selenium.By;
 
 public class landingPage extends basePage {
     By headerSignIn = By.xpath("//div[@class='menu-nav']//a[@class='signin']");
+    By enterEmail = By.xpath("//div[@role='dialog']//child::div//div//div//div//input[@placeholder='Scholastic Account Email']");
+    By enterPassword = By.xpath("//div[@class='form-row row-password-input required']//input[@type='password']");
+    By signInbutton = By.xpath("//div[@class='align-center']//button[@type='submit' and @value='Login']");
 
-    public void clickHeaderSignIn() {
-        clickThis(headerSignIn);
+
+
+    public void findHeaderSignIn (){findWebElement(headerSignIn);}
+    public void clickHeaderSignIn() {clickThis(headerSignIn);}
+    public void enterEmail(String userEmail){
+        type(enterEmail, userEmail);
+    }
+    public void enterPassword (String password){
+        type(enterPassword, password);
+
+    }
+    public void clickButton (){
+        clickThis(signInbutton);
     }
 
-    public void getAttr(){
-        getAttribute(headerSignIn, "href");
-    }
+
+
+
+
+
+
+
+
+
 }
 
