e582db55dc8981fd3614f4e21fc2b5aff1007158
Alina-lab777 <elliekuch@gmail.com>
2022-12-27 12:04:28 -0500

    Chrome driver fixed with steps

--- src/test/java/TestCases/pickCalendarDate.java
+++ src/test/java/TestCases/pickCalendarDate.java
@@ -21,10 +21,10 @@ public class pickCalendarDate {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\golde\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://hotels.com/");
-        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
 
         By idLocator = (By.id("qf-0q-destination"));
